c4e85d211943b4c0903292ba972292030497217c
Rizzo <Giancarlo.Rizzo@dlr.de>
2026-02-17 17:06:20 +0100

    space

---
+++ definitions.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+track_status_colors = {
+    "AllClear": "green",
+    "Yellow": "yellow",
+    "Red": "red",
+    "SCDeployed": "purple",
+    "VSCDeployed": "violet",
+    "VSCEnding": "orange",
+}
+
+compound_colors = {
+    'SOFT': 'red',
+    'MEDIUM': 'yellow',
+    'HARD': 'white',
+    'INTERMEDIATE': 'green',
+    'WET': 'blue'
+}
